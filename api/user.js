ccf2c4179289cf373da67b4363e06ddd46e1f88f
Ashutosh Singh <100990036+coderashu-1@users.noreply.github.com>
2025-09-11 00:19:38 +0530

    Update user.js

--- api/user.js
+++ api/user.js
@@ -8,7 +8,7 @@ const Transaction = require("../models/Transaction");
 
 // POST /api/user/ - Register Users
 router.post("/", (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password,phone} = req.body;
 
   if (!name || !email || !password) {
     return res.status(400).json({ msg: "Please enter all fields" });
@@ -17,7 +17,7 @@ router.post("/", (req, res) => {
   User.findOne({ email }).then(user => {
     if (user) return res.status(400).json({ msg: "User already exists" });
 
-    const newUser = new User({ name, email, password });
+    const newUser = new User({ name, email, password,phone });
 
     bcrypt.genSalt(10, (err, salt) => {
       if (err) throw err;
@@ -88,3 +88,4 @@ router.delete("/:id", (req, res) => {
 });
 
 module.exports = router;
+
